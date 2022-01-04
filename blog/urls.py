374c4331d305378f94ed741cb09d262251a08c0b
Your Name <you@example.com>
2022-01-05 03:46:23 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
